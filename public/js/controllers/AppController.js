053fa1a18fe0c9268e553b7926209aee3d3678e0
vincentw <vincentw@live.ca>
2015-12-02 14:43:39 -0800

    Add 404 error page. Attempt to remove hash from url.

--- public/js/controllers/AppController.js
+++ public/js/controllers/AppController.js
@@ -2,13 +2,17 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
+    $locationProvider.html5Mode(true).hashPrefix('!')
+
     $routeProvider
         .when('/', {
             templateUrl: 'views/underconstruction.html',
             controller: 'HomeController'
         })
-
-        .otherwise({redirectTo: '/'});
+        .otherwise({
+            templateUrl: 'views/404.html',
+            controller: 'HomeController'
+        });
 }])
 
     .controller('AppController', function ($scope) {
