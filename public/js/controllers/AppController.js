82e15a08760e8b4e15bc96ddee197482efef8d01
Vincent Wong <vincentw@live.ca>
2015-12-04 01:43:13 -0800

    Add angular-scroll-animate directive and animate.css lib

--- public/js/controllers/AppController.js
+++ public/js/controllers/AppController.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute', 'angular-scroll-animate']);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
@@ -12,5 +12,34 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 }])
 
     .controller('AppController', function ($scope) {
+        //leverages animate.css classes
+        $scope.animateInBounceInDown = function($el) {
+            $el.removeClass('hidden');
+            $el.addClass('animated bounceInDown'); // this example
+        };
 
+        $scope.animateOutBounceInDown = function($el) {
+            $el.addClass('hidden');
+            $el.removeClass('animated bounceInDown');
+        };
+
+        $scope.animateInPulse = function($el) {
+            $el.removeClass('hidden');
+            $el.addClass('animated pulse'); // this example
+        };
+
+        $scope.animateOutPulse = function($el) {
+            $el.addClass('hidden');
+            $el.removeClass('animated pulse');
+        };
+
+        $scope.animateInZoomInUp = function($el) {
+            $el.removeClass('hidden');
+            $el.addClass('animated zoomInUp'); // this example
+        };
+
+        $scope.animateOutZoomInUp = function($el) {
+            $el.addClass('hidden');
+            $el.removeClass('animated zoomInUp');
+        };
     });
